ad0236c4a8b795fa13f11e5053015ff08a1867bc
Juan Antonio Breña Moral <bren@juanantonio.info>
2025-08-17 21:05:53 +0200

    Starting point (#1)
    * Starting point

---
+++ src/main/kotlin/info/jab/learning/App.kt
@@ -0,0 +1,10 @@
+package info.jab.learning
+
+class App {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello World!")
+        }
+    }
+}
---
+++ src/test/kotlin/info/jab/learning/AppTest.kt
@@ -0,0 +1,11 @@
+package info.jab.learning
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertTrue
+
+class AppTest {
+    @Test
+    fun shouldAnswerWithTrue() {
+        assertTrue(true)
+    }
+}
